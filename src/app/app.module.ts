e10b10bc661a875550be897c8238123ae8ac4e3f
KeelanHoGent <keelan.savat.y2036@student.hogent.be>
2019-10-14 16:24:06 +0200

    feat(menubar): added logo and buttons
    Added logo and buttons to the menubar, these don't have any purpose atm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MenubarModule} from "./menubar/menubar.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {MenubarModule} from "./menubar/menubar.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MenubarModule
+    MenubarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menubar/menubar.module.ts
+++ src/app/menubar/menubar.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenubarContainerComponent } from './components/menubar-container/menubar-container.component';
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 
 
 @NgModule({
   declarations: [MenubarContainerComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule
   ],
   exports: [
     MenubarContainerComponent
