b10ee517f4601f4d8276d5e29b1dd71f0beea300
Keelan Savat <keelan@live.be>
2019-11-30 23:55:26 +0100

    improvement: default image when image does not exist

--- src/app/productentemplate-overzicht/product-template/product-template.component.ts
+++ src/app/productentemplate-overzicht/product-template/product-template.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { ProductTemplate } from 'src/app/types/productTemplate.model';
 import {Product} from '../../types/product.model';
 import {TemplateService} from '../../services/template.service';
+import {Observable, Subject} from "rxjs";
 
 @Component({
   selector: 'app-product-template',
@@ -12,16 +13,22 @@ export class ProductTemplateComponent implements OnInit {
 
   @Input() public productTemplate: ProductTemplate;
   @Output() public deletedProduct = new EventEmitter<ProductTemplate>();
+  public productFotoSrc = '';
 
   constructor(private templateService: TemplateService) { }
 
   ngOnInit() {
+    this.productFotoSrc = this.productTemplate.image;
   }
 
   deleteProduct(): boolean {
     this.deletedProduct.emit(this.productTemplate);
-    console.log(this.productTemplate)
+    console.log(this.productTemplate);
     this.templateService.deleteProductTemplate(this.productTemplate).subscribe();
     return false;
+  }
+
+  showDefaultImage() {
+    this.productFotoSrc = 'assets/images/image-not-found.png';
   }
 }
