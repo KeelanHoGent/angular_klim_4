34289034cc44c2f21aa86a648c603d2f937df6c9
Keelan Savat <keelan@live.be>
2019-12-02 15:01:16 +0100

    feat: made login component ready for styling

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthenticationService} from "../authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public user: FormGroup;
+  public error: string;
 
-  constructor() { }
+  constructor(
+    private authService: AuthenticationService,
+    private builder: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.user = this.builder.group({
+      username: ['',  Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.authService.login(
+      this.user.value.username,
+      this.user.value.password
+    ).subscribe(val => {
+      if (val) {
+        if (this.authService.redirectUrl) {
+          this.router.navigateByUrl(this.authService.redirectUrl);
+          this.authService.redirectUrl = undefined;
+        } else {
+          this.router.navigate(['']);
+        }
+      }
+    }, err => this.error = err.json());
   }
 
 }
